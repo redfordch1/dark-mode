46693032b0221bd637488bf4baef5aaf52b01e06
Chase Redford <chaseredford@Chases-MacBook-Pro.local>
2019-12-05 11:13:37 -0700

    Final

---
+++ src/hooks/useDarkMode.jsx
@@ -0,0 +1,21 @@
+import useLocalStorage from "./useLocalStorage";
+import { useEffect } from "react";
+
+const useDarkMode = () => {
+	const [ values, setValues ] = useLocalStorage("darkMode");
+
+	useEffect(
+		() => {
+			if (values === true) {
+				document.body.classList.add("dark-mode");
+			} else {
+				document.body.classList.remove("dark-mode");
+			}
+		},
+		[ values ]
+	);
+
+	return [ values, setValues ];
+};
+
+export default useDarkMode;
